622979a12ca5af9fd8c3e569f008bbff697f7c74
PlumeIS <79358959+PlumeIS@users.noreply.github.com>
2022-10-22 10:47:24 +0800

    Update MessageFetcher.py

--- message/MessageFetcher.py
+++ message/MessageFetcher.py
@@ -10,6 +10,8 @@ class MessageFetcher:
         self.bot = bot
         self.filters = filters
         self.messageCount = len(self.bot.getDriver().find_elements("class name", "text-message-item"))
+        self.errorCounter = 0
+        self.maxErrorCount = 20
 
     def getAllNewCount(self, messageElements):
         lastCount = self.messageCount
@@ -20,12 +22,11 @@ class MessageFetcher:
         else:
             return (self.messageCount - 100) + (200 - lastCount)
 
-    def fetch(self):
-        allMessages = self.bot.getDriver().find_elements("class name", "text-message-item")
-        readCount = self.getAllNewCount(allMessages)
-        headInfos = {}
-
-        while True:
+    def getHeadInfos(self, readCount):
+        errorCounter = 0
+        while errorCounter < self.maxErrorCount*2:
+            allMessages = self.bot.getDriver().find_elements("class name", "text-message-item")
+            headInfos = {}
             try:
                 for index, message in enumerate(allMessages[::-1]):
                     if "is-group" not in message.get_attribute("class"):
@@ -36,19 +37,60 @@ class MessageFetcher:
                         if index >= readCount:
                             break
             except StaleElementReferenceException:
-                allMessages = self.bot.getDriver().find_elements("class name", "text-message-item")
+                errorCounter += 1
+                pass
             else:
-                break
+                return headInfos
+        else:
+            raise StaleElementReferenceException
+
+    @staticmethod
+    def getChatData(message):
+        data = {"text": [], "emoji": [], "image": []}
+        try:
+            chatData = message.find_element("class name", "markdown-preview")
+        except NoSuchElementException:
+            img = message.find_element("class name", "image-type-content")
+            image = img.find_element("tag name", "img")
+            data["image"].append({image.get_attribute("src")})
+        else:
+            for i in chatData.find_elements("tag name", "span"):
+                className = i.get_attribute("class")
+                if className == "emoji-wrapper":
+                    emoji = i.find_element("tag name", "img")
+                    data["emoji"].append({"id": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
+                elif className == "image-type-content":
+                    image = i.find_element("tag name", "img")
+                    data["image"].append({image.get_attribute("src")})
+                else:
+                    if i.find_elements("tag name", "img"):
+                        emoji = i.find_element("tag name", "img")
+                        data["emoji"].append(
+                            {"view": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
+            data["text"] = chatData.text
+        return data
+
+    def fetch(self):
+        allMessages = self.bot.getDriver().find_elements("class name", "text-message-item")
+        readCount = self.getAllNewCount(allMessages)
+        headInfos = self.getHeadInfos(readCount)
 
         messageData = []
 
         if readCount == 0:
             return []
         for message in allMessages[-readCount:]:
-            try:
-                sender, date = headInfos[message.get_attribute("data-gather-id")]
-            except KeyError:
-                continue
+
+            while self.errorCounter < self.maxErrorCount:
+                try:
+                    sender, date = headInfos[message.get_attribute("data-gather-id")]
+                    break
+                except KeyError:
+                    self.errorCounter += 1
+                    headInfos = self.getHeadInfos(readCount)
+            else:
+                sender, date = ("Unknown", "今天 上午 8:00")
+            self.errorCounter += 0
 
             if sender in self.filters:
                 continue
@@ -60,27 +102,6 @@ class MessageFetcher:
             else:
                 sendTime = ConversionDate.ConversionDate(date)
 
-            data = {"text": [], "emoji": [], "image": []}
-            try:
-                chatData = message.find_element("class name", "markdown-preview")
-            except NoSuchElementException:
-                img = message.find_element("class name", "image-type-content")
-                image = img.find_element("tag name", "img")
-                data["image"].append({image.get_attribute("src")})
-            else:
-                for i in chatData.find_elements("tag name", "span"):
-                    className = i.get_attribute("class")
-                    if className == "emoji-wrapper":
-                        emoji = i.find_element("tag name", "img")
-                        data["emoji"].append({"id": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
-                    elif className == "image-type-content":
-                        image = i.find_element("tag name", "img")
-                        data["image"].append({image.get_attribute("src")})
-                    else:
-                        if i.find_elements("tag name", "img"):
-                            emoji = i.find_element("tag name", "img")
-                            data["emoji"].append(
-                                {"view": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
-                data["text"] = chatData.text
+            data = self.getChatData(message)
             messageData.append({"sender": sender, "time": sendTime.strftime("%y-%m-%d %H%M"), "data": data})
         return messageData
