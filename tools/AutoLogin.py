14961d93aafad9ec137570d0103c4516bb548d0d
PlumeIS <79358959+PlumeIS@users.noreply.github.com>
2022-11-01 19:05:40 +0800

    Update AutoLogin.py

--- tools/AutoLogin.py
+++ tools/AutoLogin.py
@@ -3,14 +3,14 @@ import json
 
 
 def AutoLogin():
-    if os.path.isfile("../config/account/autoLogin.json"):
-        with open("../config/account/autoLogin.json", 'r') as configFile:
+    if os.path.isfile("./config/account/autoLogin.json"):
+        with open("./config/account/autoLogin.json", 'r') as configFile:
             config = json.loads(configFile.read())
             if config["account"] == "":
                 return None
             return config
     else:
-        with open("../config/account/autoLogin.json", 'w') as configFile:
+        with open("./config/account/autoLogin.json", 'w') as configFile:
             configFile.write("""
 {
   "account": "",
