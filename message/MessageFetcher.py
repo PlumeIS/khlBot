76b0cf6f7651c4f8763ee50ab16a15075c599486
PlumeIS <79358959+PlumeIS@users.noreply.github.com>
2022-11-06 15:00:21 +0800

    Update MessageFetcher.py

--- message/MessageFetcher.py
+++ message/MessageFetcher.py
@@ -24,7 +24,7 @@ class MessageFetcher:
 
     def getHeadInfos(self, readCount):
         errorCounter = 0
-        while errorCounter < self.maxErrorCount*2:
+        while errorCounter < self.maxErrorCount * 2:
             allMessages = self.bot.getDriver().find_elements("class name", "text-message-item")
             headInfos = {}
             try:
@@ -47,27 +47,34 @@ class MessageFetcher:
     @staticmethod
     def getChatData(message):
         data = {"text": [], "emoji": [], "image": []}
-        try:
-            chatData = message.find_element("class name", "markdown-preview")
-        except NoSuchElementException:
-            img = message.find_element("class name", "image-type-content")
-            image = img.find_element("tag name", "img")
-            data["image"].append({image.get_attribute("src")})
-        else:
-            for i in chatData.find_elements("tag name", "span"):
-                className = i.get_attribute("class")
-                if className == "emoji-wrapper":
-                    emoji = i.find_element("tag name", "img")
-                    data["emoji"].append({"id": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
-                elif className == "image-type-content":
-                    image = i.find_element("tag name", "img")
+        textRetry = True
+        while textRetry:
+            try:
+                textRetry = False
+                chatData = message.find_element("class name", "markdown-preview")
+            except NoSuchElementException:
+                try:
+                    img = message.find_element("class name", "image-type-content")
+                    image = img.find_element("tag name", "img")
                     data["image"].append({image.get_attribute("src")})
-                else:
-                    if i.find_elements("tag name", "img"):
+                except NoSuchElementException:
+                    textRetry = True
+            else:
+
+                for i in chatData.find_elements("tag name", "span"):
+                    className = i.get_attribute("class")
+                    if className == "emoji-wrapper":
                         emoji = i.find_element("tag name", "img")
-                        data["emoji"].append(
-                            {"view": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
-            data["text"] = chatData.text
+                        data["emoji"].append({"alt": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
+                    elif className == "image-type-content":
+                        image = i.find_element("tag name", "img")
+                        data["image"].append(image.get_attribute("src"))
+                    else:
+                        if i.find_elements("tag name", "img"):
+                            emoji = i.find_element("tag name", "img")
+                            data["emoji"].append(
+                                {"alt": emoji.get_attribute("alt"), "url": emoji.get_attribute("src")})
+                data["text"] = chatData.text
         return data
 
     def fetch(self):
@@ -79,29 +86,40 @@ class MessageFetcher:
 
         if readCount == 0:
             return []
-        for message in allMessages[-readCount:]:
 
-            while self.errorCounter < self.maxErrorCount:
-                try:
-                    sender, date = headInfos[message.get_attribute("data-gather-id")]
+        retry = True
+        stopFlag = False
+        while retry:
+            for message in allMessages[-readCount:]:
+                if stopFlag:
                     break
-                except KeyError:
-                    self.errorCounter += 1
-                    headInfos = self.getHeadInfos(readCount)
-            else:
-                sender, date = ("Unknown", "今天 上午 8:00")
-            self.errorCounter += 0
+                while self.errorCounter < self.maxErrorCount:
+                    try:
+                        sender, date = headInfos[message.get_attribute("data-gather-id")]
+                        retry = False
+                        break
+                    except KeyError:
+                        self.errorCounter += 1
+                        headInfos = self.getHeadInfos(readCount)
+                    except StaleElementReferenceException:
+                        allMessages = self.bot.getDriver().find_elements("class name", "text-message-item")
+                        stopFlag = True
+                        continue
+                else:
+                    sender, date = ("Unknown", "今天 上午 8:00")
+                self.errorCounter += 0
 
-            if sender in self.filters:
-                continue
+                if sender in self.filters:
+                    continue
 
-            smallTime = message.find_elements("class name", "message-small-time")
-            if smallTime:
-                date = " ".join(date.split(" ")[:2]) + f' {smallTime[0].get_attribute("innerHTML")}'
-                sendTime = ConversionDate.ConversionDate(date)
-            else:
-                sendTime = ConversionDate.ConversionDate(date)
+                smallTime = message.find_elements("class name", "message-small-time")
+                if smallTime:
+                    date = " ".join(date.split(" ")[:2]) + f' {smallTime[0].get_attribute("innerHTML")}'
+                    sendTime = ConversionDate.ConversionDate(date)
+                else:
+                    sendTime = ConversionDate.ConversionDate(date)
 
-            data = self.getChatData(message)
-            messageData.append({"sender": sender, "time": sendTime.strftime("%y-%m-%d %H:%M"), "data": data})
+                data = self.getChatData(message)
+                messageData.append(
+                    {"sender": sender, "server": self.bot.server, "channel": self.bot.channel, "time": sendTime.strftime("%y-%m-%d %H:%M"), "data": data})
         return messageData
